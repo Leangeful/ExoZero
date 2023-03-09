3afb8e3a6ad81fbfd251661eb531ee1a7579b88d
Leange <cuttheleash1@gmail.com>
2023-03-10 00:52:57 +0100

    changed dep

--- src/lib/server/oauth/google.ts
+++ src/lib/server/oauth/google.ts
@@ -1,16 +1,16 @@
 import {
-	OUATH_GOOGLE_CLIENT_ID,
-	OUATH_GOOGLE_CLIENT_SECRET,
-	OUATH_GOOGLE_REDIRECT_URI
+	OAUTH_GOOGLE_CLIENT_ID,
+	OAUTH_GOOGLE_CLIENT_SECRET,
+	OAUTH_GOOGLE_REDIRECT_URI
 } from '$env/static/private';
 
 import { google } from '@lucia-auth/oauth/providers';
 import { auth } from '$lib/server/lucia';
 
 export const googleAuth = google(auth, {
-	clientId: OUATH_GOOGLE_CLIENT_ID,
-	clientSecret: OUATH_GOOGLE_CLIENT_SECRET,
-	redirectUri: OUATH_GOOGLE_REDIRECT_URI,
+	clientId: OAUTH_GOOGLE_CLIENT_ID,
+	clientSecret: OAUTH_GOOGLE_CLIENT_SECRET,
+	redirectUri: OAUTH_GOOGLE_REDIRECT_URI,
 	scope: [
 		'https://www.googleapis.com/auth/userinfo.email',
 		'https://www.googleapis.com/auth/userinfo.profile'
