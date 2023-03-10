a8d5a8a58f2f7f106cc61b28ee651d75935d1ec6
Leange <cuttheleash1@gmail.com>
2023-03-10 20:25:15 +0100

    more test stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,41 +10,39 @@
 {:else}
 	<div class="grid">
 		<div class="row">
-			<div class="disp col-sm-4">
-				<img src="/ufo_alpha2.png" alt="ufo wireframe" />
+			<div class="col-md-4">
+				<div class="disp">
+					<img src="/ufo_alpha2.png" alt="ufo wireframe" />
+				</div>
 			</div>
-			<div class="disp col-sm-4">
-				<img class="filtered" src="/ufo_alpha2.png" alt="ufo wireframe" />
+			<div class="col-md-4">
+				<div class="disp">
+					<img class="filtered" src="/ufo_alpha2.png" alt="ufo wireframe" />
+				</div>
 			</div>
-			<div class="disp col-sm-4">
-				<img src="/ufo_alpha2.png" alt="ufo wireframe" />
-			</div>
-		</div>
-		<div class="row">
-			<div class="disp col-sm-4">
-				<img src="/ufo_alpha2.png" alt="ufo wireframe" />
-			</div>
-			<div class="disp col-sm-4">
-				<img src="/ufo_alpha2.png" alt="ufo wireframe" />
-			</div>
-			<div class="disp col-sm-4">
-				<img class="filtered" src="/ufo_alpha2.png" alt="ufo wireframe" />
+			<div class="col-md-4">
+				<div class="disp">
+					<img class="filtered" src="/ufo_alpha2.png" alt="ufo wireframe" />
+				</div>
 			</div>
 		</div>
 	</div>
 {/if}
 
 <style>
-	img {
+	.disp {
 		width: 100%;
 		height: auto;
-	}
-	.disp {
-		height: auto;
 		border: 0.1rem solid var(--ez-color);
+		margin: 0.1%;
 	}
 
 	.filtered {
 		filter: sepia() hue-rotate(180deg) saturate(2000%);
+	}
+
+	img {
+		width: 100%;
+		height: auto;
 	}
 </style>
